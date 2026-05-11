bdc3898b6c6ad7a9104e3d5dab8a842c845c7560
doni <toodol12@gmail.com>
2026-05-11 23:40:27 +0900

    feat: add shared types, constants, and date util

---
+++ 소박이/src/constants/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  cream: '#FAF6EE',
+  warmWhite: '#FFFDF8',
+  oliveGreen: '#6B7C4A',
+  oliveDark: '#4A5C2F',
+  wood: '#8B6F47',
+  woodLight: '#C4A882',
+  text: '#3D3020',
+  textMuted: '#8C7B6B',
+  surface: '#F2EBE0',
+} as const;
